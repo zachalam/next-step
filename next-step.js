f13dc00252d8d41ded6349735f1d5784eb5fa2f5
Zach Alam <zelphics@gmail.com>
2015-07-04 14:38:58 -0700

    revamped nextstep

--- next-step.js
+++ next-step.js
@@ -1,82 +1,73 @@
-/**
-	Project: Next Step
-	Filename: nextstep.js
-	Description: A jQuery extension that turns a series of 
-	hidden div layers into transitionable slides.
-	Author: Zach Alam
-	Example: http://everbranded.com/order
-*/
-
 $(document).ready(function() {
 
-	// DOM is ready for manipulation
-	// find out how many steps there are in this sequence
-	var total_steps = 1;
-	while($(".step-"+total_steps).length) total_steps++;
-
-
-	// sets the progress bar level
-	function set_progress(curr_step)
+	// determines how many steps are in the presentation
+	function total_nextstep_count()
 	{
-		// calculate the % complete for <progress> tag.
-		var progress = (100 / total_steps) * curr_step;
-		$(".step-progress").val(progress);
+		var step_count = 1;
+		while($("#nextstep-"+step_count).length)
+		{
+			// increment step count
+			step_count++;
+		}
+		
+		// return step_count
+		return step_count - 1;
 	}
 
+	function toggle_nextstep_nav()
+	{
+		// hide the back button if this is the first step
+		if(window.current_step == 1) $(".nextstep-back").hide();
+		else $(".nextstep-back").show();
+		
+		// hide the next button if this is the last step
+		var total_step_count = total_nextstep_count();
+		if(total_step_count == window.current_step) $(".nextstep-next").hide();
+		else $(".nextstep-next").show();
+	}
 
-	// set step counter to 1
-	var current_step = 1;
-	// set progress bar to current step
-	set_progress(current_step);
+	// -------------------------------------------------
 
-	// move to NEXT step
-	$(".step-next").on("click",function() {
-
-		// check if callback exists, if one does execute it.
-		// the callback must return true to continue with progress.
-		var proceed = true;
-		var callback = $(".step-"+current_step).attr("data-callback");
-		if(callback) proceed = window[callback](); 
-
-		if(proceed)
-		{
-			// we can proceed with the transition.
-			$(".step-"+current_step).slideUp(function() {
-
-				// find next step and toggle
-				current_step++;
-				$(".step-"+current_step).slideDown();
-
-				// set progress forward one step
-				set_progress(current_step);
-
-				// if current step isn't first, show back button
-				if(current_step > 1) { $(".step-prev").show(); }
-
-			});
-
-		} 
-
-	});
-
-
-	// move to PREV step
-	$(".step-prev").on("click",function() {
-
-		// toggle current step
-		$(".step-"+current_step).slideUp(function() {
-
-			// find previous step and toggle
-			current_step--;
-			$(".step-"+current_step).slideDown();
-
-			// set progress back one step
-			set_progress(current_step);
-
-			// if current step is first, hide back button
-			if(current_step == 1) { $(".step-prev").hide(); }
+	function nextstep_next()
+	{
+		console.log(window.current_step);
+		// process next step 
+		$("#nextstep-"+window.current_step).fadeOut(function() {
+			// increment current_step.
+			window.current_step++;
+			// fadein nextstep div.
+			$("#nextstep-"+window.current_step).fadeIn();
+			
+			// toggle nav
+			toggle_nextstep_nav();		
+			
 		});
+		
 
-	});
+		
+	}
+	
+	function nextstep_back()
+	{
+		$("#nextstep-"+window.current_step).fadeOut(function() {
+			// increment current_step.
+			window.current_step--;
+			// fadein previous step div.
+			$("#nextstep-"+window.current_step).fadeIn();
+			
+			// toggle nav
+			toggle_nextstep_nav();		
+			
+		});
+	}
+
+	// script startup
+	// set the current step as 1
+	window.current_step = 1;
+	toggle_nextstep_nav();	
+
+	// listeners for actions
+	$(".nextstep-next").on("click",nextstep_next);
+	$(".nextstep-back").on("click",nextstep_back);
 
 });
